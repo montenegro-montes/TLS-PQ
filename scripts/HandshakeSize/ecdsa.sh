0643d07a5639053ba38ca862dd4f86e3f4d90c58
José Antonio Montenegro Montes <126468995+montenegro-montes@users.noreply.github.com>
2024-10-03 13:51:25 +0200

    Delete scripts directory

--- scripts/HandshakeSize/ecdsa.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -e KEM_ALG=P-384 -e SIG_ALG=secp384r1 -it oqs-curlv331 perftestHandshakeECDSA.sh
-docker run -e KEM_ALG=kyber768 -e SIG_ALG=secp384r1 -it oqs-curlv331 perftestHandshakeECDSA.sh
