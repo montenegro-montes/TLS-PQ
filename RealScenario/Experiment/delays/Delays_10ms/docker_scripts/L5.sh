0b266ace0cd4f3a3d6b4cebc1dced8ea8781967e
José Antonio Montenegro Montes <126468995+montenegro-montes@users.noreply.github.com>
2024-11-23 21:25:57 +0100

    Add files via upload

---
+++ RealScenario/Experiment/delays/Delays_10ms/docker_scripts/L5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "secp521r1"
+
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10  -e KEM_ALG=P-521     -e SIG_ALG=secp521r1 -it oqs-curlv331_tc perftestServer.sh
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10  -e KEM_ALG=mlkem1024 -e SIG_ALG=secp521r1 -it oqs-curlv331_tc perftestServer.sh
+
+echo "mldsa87"
+
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10 -e KEM_ALG=P-521      -e SIG_ALG=mldsa87 -it oqs-curlv331_tc perftestServer.sh
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10 -e KEM_ALG=mlkem1024  -e SIG_ALG=mldsa87 -it oqs-curlv331_tc perftestServer.sh
+
+echo "falcon1024"
+
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10 -e KEM_ALG=P-521     -e SIG_ALG=falcon1024 -it oqs-curlv331_tc perftestServer.sh
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10 -e KEM_ALG=mlkem1024 -e SIG_ALG=falcon1024 -it oqs-curlv331_tc perftestServer.sh
+
+echo "sphincssha2256fsimple"
+
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10  -e KEM_ALG=P-521     -e SIG_ALG=sphincssha2256fsimple -it oqs-curlv331_tc perftestServer.sh
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10  -e KEM_ALG=mlkem1024 -e SIG_ALG=sphincssha2256fsimple -it oqs-curlv331_tc perftestServer.sh
+
+echo "sphincssha2256ssimple"
+
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10  -e KEM_ALG=P-521     -e SIG_ALG=sphincssha2256ssimple -it oqs-curlv331_tc perftestServer.sh
+docker run --cap-add=NET_ADMIN  -e TC_DELAY=10ms -e TC_LOSS=0%  -e TEST_TIME=10  -e KEM_ALG=mlkem1024 -e SIG_ALG=sphincssha2256ssimple -it oqs-curlv331_tc perftestServer.sh
