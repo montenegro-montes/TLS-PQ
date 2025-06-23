2910fce594aa580da2f6c30633e6c7529a532407
José Antonio Montenegro Montes <126468995+montenegro-montes@users.noreply.github.com>
2025-06-23 12:17:55 +0200

    Add files via upload

---
+++ Framework/Legacy/RealScenario/Docker/perftestHandshakeMutual.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+set -e
+
+if [ "x$TC_DELAY" == "x" ]; then
+    $TC_DELAY = 0ms
+fi    
+
+if [ "x$TC_LOSS" == "x" ]; then
+    $TC_LOSS = "0%"
+fi 
+
+echo "tc"
+tc qdisc add dev lo root netem delay $TC_DELAY loss $TC_LOSS
+
+echo "Showing qdisc status for the interface: lo"
+tc -s qdisc show dev lo
+
+
+# Optionally set KEM to one defined in https://github.com/open-quantum-safe/oqs-provider#algorithms
+if [ "x$KEM_ALG" == "x" ]; then
+	export DEFAULT_GROUPS=kyber512
+else
+        export DEFAULT_GROUPS=$KEM_ALG
+fi
+
+# Optionally set SIG to one defined in https://github.com/open-quantum-safe/oqs-provider#algorithms
+if [ "x$SIG_ALG" == "x" ]; then
+	export SIG_ALG=dilithium2
+fi
+
+
+
+# Optionally set server certificate alg to one defined in https://github.com/open-quantum-safe/oqs-provider#algorithms
+# The root CA's signature alg remains as set when building the image
+if [ "x$SIG_ALG" != "x" ]; then
+    cd /opt/oqssa/bin
+
+    openssl req -x509 -new -newkey $SIG_ALG -keyout /opt/test/CA.key -out /opt/test/CA.crt -nodes -subj "/CN=oqstest CA" -days 365 -config /opt/oqssa/ssl/openssl.cnf
+
+    # generate new server CSR using  CA.key & cert
+    openssl req -new -newkey $SIG_ALG -keyout /opt/test/server.key -out /opt/test/server.csr -nodes -subj "/CN=localhost"
+    if [ $? -ne 0 ]; then
+       echo "Error generating keys - aborting."
+       exit 1
+    fi
+    # generate server cert
+    openssl x509 -req -in /opt/test/server.csr -out /opt/test/server.crt -CA /opt/test/CA.crt -CAkey /opt/test/CA.key -CAcreateserial -days 365
+    if [ $? -ne 0 ]; then
+       echo "Error generating cert - aborting."
+       exit 1
+    fi
+
+   # generate new user CSR using  CA.key & cert
+    openssl req -new -newkey $SIG_ALG -keyout /opt/test/user.key -out /opt/test/user.csr -nodes -subj "/CN=user"
+    if [ $? -ne 0 ]; then
+       echo "Error generating keys - aborting."
+       exit 1
+    fi
+    # generate server cert
+    openssl x509 -req -in /opt/test/user.csr -out /opt/test/user.crt -CA /opt/test/CA.crt -CAkey /opt/test/CA.key -CAcreateserial -days 365
+    if [ $? -ne 0 ]; then
+       echo "Error generating cert - aborting."
+       exit 1
+    fi
+
+fi
+
+echo "Running $0 with SIG_ALG=$SIG_ALG and KEM_ALG=$KEM_ALG"
+echo
+
+# Start a TLS1.3 test server based on OpenSSL accepting only the specified KEM_ALG
+# The env var DEFAULT_GROUPS activates the required Group via the system openssl.cnf:
+# we put it on the command line to check for possible typos otherwise silently discarded:
+openssl s_server -cert /opt/test/server.crt -key /opt/test/server.key -groups $DEFAULT_GROUPS -www -tls1_3 -accept localhost:4433 -verify 1 -verifyCAfile /opt/test/CA.crt &
+
+# Give server time to come up first:
+sleep 1
+
+# Run handshakes for $TEST_TIME seconds
+# The env var DEFAULT_GROUPS activates the required Group via the system openssl.cnf:
+openssl s_client -connect localhost:4433 -state -servername localhost -CAfile /opt/test/CA.crt -showcerts -cert /opt/test/user.crt -key /opt/test/user.key
